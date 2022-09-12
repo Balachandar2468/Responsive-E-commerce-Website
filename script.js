d608f1ecd5ad083b4296b33d94fb20955d12555b
Bala Chandar <chanbala555@gmail.com>
2022-09-12 12:30:23 +0530

    Commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const bar=document.getElementById("bar")
+const nav=document.getElementById("navbar")
+const close=document.getElementById("close")
+
+if(bar){
+    bar.addEventListener("click",function(){
+        nav.classList.add("active")
+    })
+}
+
+if(close){
+    close.addEventListener("click",function(){
+        nav.classList.remove("active")
+    })
+}
+
